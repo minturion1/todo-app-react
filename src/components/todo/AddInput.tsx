8055bcc4f96264ab255285c6dee76095d138b329
Roman <romapotochniak@gmail.com>
2026-04-26 17:43:39 +0200

    add input changed

--- src/components/todo/AddInput.tsx
+++ src/components/todo/AddInput.tsx
@@ -18,7 +18,7 @@ const AddInput = (props: Props) => {
     return (
         <div className="flex gap-5">
             <Input value={value} setValue={setValue} onEnter={handleAdd}/>
-            <button onClick={handleAdd} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" >
+            <button onClick={handleAdd} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition" >
                 Add
             </button>
         </div>
